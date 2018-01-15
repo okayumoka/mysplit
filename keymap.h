6803f2811973236aecc68bc80f8955979c010dbf
okayu <moka1988jp@gmail.com>
2018-01-15 17:38:03 +0900

    making

--- keymap.h
+++ keymap.h
@@ -1,24 +1,20 @@
 #ifndef __INCLUDED_KEYMAP_H__
 #define __INCLUDED_KEYMAP_H__
 
+#include "keycode.h"
+
 // const int rowNum = 5;
 // const int colNum = 6;
+// const int colNum2 = 12;
 
-// LeftSide
-const byte keyMapL[rowNum*2][colNum]  = {
-	{KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5    },
-	{KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T    },
-	{KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G    },
-	{KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B    },
-	{KC_NULL, KC_LALT, KC_LGUI, KC_LSFT, KC_SPC,  KC_NULL }
-};
-// RightSide
-const byte keyMapR[rowNum][colNum]  = {
-	{KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL  },
-	{KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL  },
-	{KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
-	{KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS },
-	{KC_NULL, KC_BSPC, KC_ENT,  KC_RGUI, KC_BSLS, KC_NULL }
+const byte keyMapL[rowNum][colNum2]  = {
+	/* --------------------------------- LEFT side ----------|------- RIGHT side ------------------------------------ */
+	{KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /*|*/ KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL  },
+	{KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    /*|*/ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL  },
+	{KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    /*|*/ KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
+	{KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    /*|*/ KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS },
+	{KC_NULL, KC_LALT, KC_LGUI, KC_LSFT, KC_SPC,  KC_NULL, /*|*/ KC_NULL, KC_BSPC, KC_ENT,  KC_RGUI, KC_BSLS, KC_NULL }
+	/* --------------------------------- LEFT side ----------|------- RIGHT side ------------------------------------ */
 };
 
 #endif
--- mysplit.ino
+++ mysplit.ino
@@ -1,24 +1,30 @@
+// MySplit
+// ホスト（左手側）用
+
 #include "Keyboard.h"
-#include "keycode.h"
 
 const int rowNum = 5;
 const int colNum = 6;
+const int colNum2 = 12;
 
 #include "keymap.h"
 
 const int[rowNum] rowPins = { 3, 4, 5, 6, 7 };
 const int[colNum] colPins = { 8, 9, 10, 11, 12 };
 
-bool currentState[rowNum][colNum];
-bool prevState[rowNum][colNum];
-
-int i;
-int j;
+bool currentState[rowNum][colNum2];
+bool prevState[rowNum][colNum2];
 
 // 初期化
 void setup() {
+	int i;
+	int j;
+
 	// デバッグ用のメッセージに使用
 	Serial.begin(9600);
+
+	// 左右の通信に使用
+	Serial1.begin(9600);
 
 	// ピンの初期化
 	for (i = 0; i < rowNum; i++) {
@@ -34,16 +40,73 @@ void setup() {
 
 	// 状態の初期化
 	for (i = 0; i < rowNum; i++) {
-		for (j = 0; j < colNum; j++) {
+		for (j = 0; j < colNum2; j++) {
 			currentState[i][j] = HIGH;
 			prevState[i][j] = HIGH;
 		}
 	}
+
+	// キーボード処理開始
+	Keyboard.begin();
 }
 
 void loop() {
+	// ホストの押下状態を取得
+	getHostState();
+	// サブの押下状態を取得
+	getSubState();
+	// キー押下処理
+	applyKeyState();
+}
+
+void getHostState() {
+	int i;
+	int j;
 	for (i = 0; i < rowNum; i++) {
+		int rowPin = rowPins[i];
+		digitalWrite(rowPin, LOW);
 		for (j = 0; i < colNum; j++) {
+			int colPin = colPins[i];
+			currentState[i][j] = digitalRead(colPin);
+		}
+	}
+}
+
+void getSubState() {
+	if (!Serial1.available()) return;
+
+	int readData = Serial1.read();
+	if (readData == -1) return;
+
+	// 通信方式について
+	// 0bABCDEFGH
+	// FGH ... colPinの番号
+	// CDE ... rowPinの番号
+	// B   ... Press時は0、Release時は1
+	// A   ... 未使用
+
+	bool isPress = (bool) ((readData & 0x10000000 >> 7) == 0);
+	bool isLeft  = (bool) ((readData & 0x01000000 >> 6) == 0);
+	int rowPin   = (int)  (readData & 0x00111000 >> 3);
+	int colPin   = (int)  (readData & 0x00000111);
+
+	// 右手用
+	currentState[rowPin][colPin + colNum] = isPress ? LOW : HIGH;
+}
+
+void applyKeyState() {
+	int i;
+	int j;
+	for (i = 0; i < rowNum; i++) {
+		for (j = 0; j < colNum2; j++) {
+			if (currentState[i][j] != prevState[i][j]) {
+				if (currentState[i][j] == LOW) {
+					Keyboard.press(keyMap[i][j]);
+				} else {
+					Keyboard.release(keyMap[i][j]);
+				}
+			}
+			prevState[i][j] = currentState[i][j];
 		}
 	}
 }
